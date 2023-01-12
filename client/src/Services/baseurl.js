3237595e711267fe920e8c0538b06a0eb06d9e64
rohit.pandit <rohit.pandit@indicchain.com>
2023-01-12 09:56:26 +0530

    update the port

--- client/src/Services/baseurl.js
+++ client/src/Services/baseurl.js
@@ -1,2 +1,3 @@
-export const base_url = "http://172.16.0.84:5029/api/v1/";
+// export const base_url = "http://172.16.0.84:5029/api/v1/";
+export const base_url = "http://49.248.38.51:5029/api/v1/";
 // export const base_url = "http://localhost:5000/api/v1/";
