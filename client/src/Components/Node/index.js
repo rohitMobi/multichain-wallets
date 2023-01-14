b811acb549cb5e50e606b7d17fc5d0918410a87c
RohitPandit <panditrohit532@gmail.com>
2023-01-14 07:27:44 +0530

    Add Modal for node information.

--- client/src/Components/Node/index.js
+++ client/src/Components/Node/index.js
@@ -4,6 +4,7 @@ import { getInfo } from "../../Services/api";
 const NodeCompoenent = () => {
 
   const [list, setList] = useState({})
+  const [modal, setModal] = useState(false)
 
   useEffect(() => {
     getList();
@@ -23,6 +24,9 @@ const NodeCompoenent = () => {
           <div className="row mb-2">
             <div className="col-sm-6">
               <h1 className="m-0 text-white"> Node Information </h1>
+            </div>
+            <div className="col-sm-6">
+              <button className="btn float-right" onClick={() => { setModal(true) }}><i class="fa fa-question-circle text-white" aria-hidden="true"></i></button>
             </div>
           </div>
           <div className="row mt-3">
@@ -66,6 +70,27 @@ const NodeCompoenent = () => {
           </div>
         </div>
       </div>
+      {
+        modal == true && <>
+          <div className="modal">
+            <div className="card text-white" style={{ background: "rgba(255, 255, 255, 0.2)", width: "50%" }}>
+              
+              <div className="left-gold"></div>
+              <div className="card-header">
+                  <h3>Node Information</h3>
+              </div>
+              <div className="card-body">
+                In the node information section show all the information about the node. In the section name represents the name of the chain we has to created.
+                Version represents the version of the multichain. Protocol information show the which protocol use to create the chain. Node Address show the address of the node.
+                Blocks represents the how many blocks we have to create on the chain. Peers represent the number of peers connected to the chain
+              </div>
+              <div className="card-footer">
+                <button className="btn text-white float-right" onClick={() => { setModal(false) }}>Close</button>
+              </div>
+            </div>
+          </div>
+        </>
+      }
     </>
   );
 };
